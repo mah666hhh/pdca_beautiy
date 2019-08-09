86ab46cd10ad6362f08d481e1d74119049e5cf64
masarutakeyoshi <mah666hhh@gmail.com>
2019-08-09 21:13:27 +0900

    modified order of execute seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(BooksTableSeeder::class);
-        $this->call(PostsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(PostsTableSeeder::class);
     }
 }
